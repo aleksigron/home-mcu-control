a7d9149d90c3f7cea4b6559c5d23503770bb4d71
Aleksi Grön <aleksi.gron@gmail.com>
2020-04-26 19:29:05 +0300

    [server] Continue listening to connections after a disconnect

--- device-server/src/DeviceServer.c
+++ device-server/src/DeviceServer.c
@@ -15,23 +15,24 @@
 enum { BufferSize = 128 };
 const uint16_t port = 3123;
 
+struct DeviceServer
+{
+	int listenSocket;
+	int openSocket;
+};
+
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
 
-int DeviceServer_run()
+static void initListenSocket(struct DeviceServer* server)
 {
-	int sockfd, newsockfd;
-	socklen_t clientLen;
-	unsigned char buffer[BufferSize];
-	struct sockaddr_in serverAddr, clientAddr;
-	ssize_t readBytes, writeBytes;
-	unsigned short currentMessageNumber = 0;
+	struct sockaddr_in serverAddr;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
+	server->listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server->listenSocket < 0)
 		error("ERROR opening socket");
 
 	bzero((char *) &serverAddr, sizeof(serverAddr));
@@ -39,98 +40,128 @@ int DeviceServer_run()
 	serverAddr.sin_addr.s_addr = INADDR_ANY;
 	serverAddr.sin_port = htons(port);
 
-	if (bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+	int bindRes = bind(server->listenSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+
+	if (bindRes < 0)
 		error("ERROR on binding");
 
-	listen(sockfd,5);
+	int connectionBacklog = 5;
+	listen(server->listenSocket, connectionBacklog);
+}
+
+static void waitForConnection(struct DeviceServer* server)
+{
+	socklen_t clientLen;
+	struct sockaddr_in clientAddr;
+
 	clientLen = sizeof(clientAddr);
-	newsockfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientLen);
-	if (newsockfd < 0)
+	server->openSocket = accept(server->listenSocket, (struct sockaddr*)&clientAddr, &clientLen);
+
+	if (server->openSocket < 0)
 		error("ERROR on accept");
+}
+
+static void closeSockets(struct DeviceServer* server)
+{
+	close(server->openSocket);
+	close(server->listenSocket);
+}
+
+int DeviceServer_run()
+{
+	struct DeviceServer server;
+	unsigned char buffer[BufferSize];
+	ssize_t readBytes, writeBytes;
+	unsigned short currentMessageNumber = 0;
+
+	initListenSocket(&server);
 
 	while (1)
 	{
-		bzero(buffer, BufferSize);
-		readBytes = read(newsockfd, buffer, BufferSize - 1);
+		waitForConnection(&server);
 
-		printf("Read bytes: %d\n", (int)readBytes);
-
-		const uint8_t* bufferItr = buffer;
-		const uint8_t* bufferEnd = buffer + readBytes;
-
-		while (bufferItr < bufferEnd)
+		while (1)
 		{
-			const uint8_t* messageStart = DeviceProtocol_findMessageStart(bufferItr, bufferEnd);
+			bzero(buffer, BufferSize);
+			readBytes = read(server.openSocket, buffer, BufferSize - 1);
 
-			// We found message start
-			if (messageStart != NULL)
+			if (readBytes < 0)
 			{
-				uint8_t messageLength = DeviceProtocol_getMessageLength(messageStart);
-				uint8_t messageType = DeviceProtocol_getMessageType(messageStart);
-
-				// TODO: if (messageStart + messageLength > READ_BUFFER_SIZE)
-
-
-				if (messageType == MsgType_Acknowledge)
-				{
-					struct MessageAcknowledge acknowledge;
-					DeviceProtocol_readMessageAcknowledge(messageStart, &acknowledge);
-
-					printf("Received acknowledge to message %d\n", (int)acknowledge.responseTo);
-				}
-
-				if (messageType == MsgType_Connected)
-				{
-					struct MessageConnected connected;
-					DeviceProtocol_readMessageConnected(messageStart, &connected);
-
-					printf("Received connected message from device %d\n", (int)connected.deviceName);
-				}
-
-				bufferItr = messageStart + messageLength;
+				printf("Error reading from socket: %d\n", (int)readBytes);
+				break;
 			}
-			else
+
+			printf("Read bytes: %d\n", (int)readBytes);
+
+			const uint8_t* bufferItr = buffer;
+			const uint8_t* bufferEnd = buffer + readBytes;
+
+			while (bufferItr < bufferEnd)
 			{
-				bufferItr = bufferEnd;
+				const uint8_t* messageStart = DeviceProtocol_findMessageStart(bufferItr, bufferEnd);
+
+				// We found message start
+				if (messageStart != NULL)
+				{
+					uint8_t messageLength = DeviceProtocol_getMessageLength(messageStart);
+					uint8_t messageType = DeviceProtocol_getMessageType(messageStart);
+
+					// TODO: if (messageStart + messageLength > READ_BUFFER_SIZE)
+
+					if (messageType == MsgType_Acknowledge)
+					{
+						struct MessageAcknowledge acknowledge;
+						DeviceProtocol_readMessageAcknowledge(messageStart, &acknowledge);
+
+						printf("Received acknowledge to message %d\n", (int)acknowledge.responseTo);
+					}
+
+					if (messageType == MsgType_Connected)
+					{
+						struct MessageConnected connected;
+						DeviceProtocol_readMessageConnected(messageStart, &connected);
+
+						printf("Received connected message from device %d\n", (int)connected.deviceName);
+					}
+
+					bufferItr = messageStart + messageLength;
+				}
+				else
+				{
+					bufferItr = bufferEnd;
+				}
 			}
-		}
 
-		if (readBytes < 0)
-		{
-			error("ERROR reading from socket");
-			break;
-		}
+			sleep(5);
 
-		sleep(5);
+			{
+				struct MessageSetLighting message;
+				message.number = currentMessageNumber;
+				message.animationType = (uint8_t)(rand() % 3);
+				message.animationSpeed = 5;
+				message.hue = (uint8_t)(rand() % 256);
+				message.brightness = (uint8_t)(rand() % 32);
 
-		{
-			struct MessageSetLighting message;
-			message.number = currentMessageNumber;
-			message.animationType = (uint8_t)(rand() % 3);
-			message.animationSpeed = 5;
-			message.hue = (uint8_t)(rand() % 256);
-			message.brightness = (uint8_t)(rand() % 32);
+				DeviceProtocol_writeMessageSetLighting(buffer, &message);
 
-			DeviceProtocol_writeMessageSetLighting(buffer, &message);
+				currentMessageNumber += 1;
+			}
 
-			currentMessageNumber += 1;
-		}
+			printf("Sending MessageType_Set, animation %d, hue %d, brightness %d\n",
+				   (int)buffer[MsgPos_Animation],
+				   (int)buffer[MsgPos_Hue],
+				   (int)buffer[MsgPos_Brightness]);
 
-		printf("Sending MessageType_Set, animation %d, hue %d, brightness %d\n",
-			(int)buffer[MsgPos_Animation],
-			(int)buffer[MsgPos_Hue],
-			(int)buffer[MsgPos_Brightness]);
-
-		writeBytes = write(newsockfd, &buffer, MsgLen_SetLighting);
-		if (writeBytes < 0)
-		{
-			error("ERROR writing to socket");
-			break;
+			writeBytes = write(server.openSocket, &buffer, MsgLen_SetLighting);
+			if (writeBytes < 0)
+			{
+				printf("Error writing to socket: %d\n", (int)writeBytes);
+				break;
+			}
 		}
 	}
 
-	close(newsockfd);
-	close(sockfd);
+	closeSockets(&server);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
